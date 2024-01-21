40bd8b5b4c84f19b7de3d04ea760c8d96a7f5509
gm0877 <gm0877@srmist.edu.in>
2024-01-21 23:20:56 +0530

    first commit

---
+++ functions.js
@@ -0,0 +1,63 @@
+//IF else 
+// i=87
+// if (i==88){
+//     console.log(i)
+// }
+// else if (i==87){
+//     console.log(`${i} is a valid number`)
+// }
+// else{
+//     console.log(`${i}not a valid number`)
+// }
+//switch
+// let a=1
+// switch(a){
+//     case 0:
+//         console.log("Zero");
+//         break
+    
+//     case 1:
+//         console.log("One")
+//         break
+    
+//     case 2:
+//         console.log("Two");
+//         break
+    
+//     case 3:
+//         console.log("Three");
+//         break
+        
+// }
+// let b=1
+// switch(b){
+//     case 0:
+//         var c='21'
+//         console.log(c)
+//     case 1:
+//         console.log(25)
+//         console.log(c)
+//         console.log(typeof c)
+//     case 2:
+//         console.log(35)
+//         break
+//     case 3:
+//         console.log(56)
+
+//}
+// let a=10;
+// let b=10;
+// console.log(a+b);
+//   ------------------Arrow Function--------------
+// const myFunction = () => {
+//     # function body
+// };
+// const geet=()=>{
+//     console.log('hi this is mani')
+// }
+// geet()
+
+sai()
+function sai(){
+    console.log("hello")
+}
