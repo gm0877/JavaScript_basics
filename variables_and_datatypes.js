b9f34b686ffa6e5aa85d5fd4bd780b70ed8e34ff
gm0877 <gm0877@srmist.edu.in>
2024-01-21 23:25:28 +0530

    first commit

---
+++ variables_and_datatypes.js
@@ -0,0 +1,67 @@
+// let g=1223
+// function sai(){
+//     console.log('sai');
+// }
+// sai();
+
+// let a=10
+// function type(){
+//     b=7
+//     console.log('hello',b1);    
+// }
+//  a=8
+// function hello(){
+//     console.log(a)
+//     a=9;
+//     console.log(a);
+//     a=88;
+//     console.log(a);
+// }
+// hello();
+// console.log(a);
+
+// var c=9*9
+// function ump(){
+//     console.log(c);
+//     c=8*8;
+//     console.log(c);
+
+// }
+// ump();
+ 
+// function types(){
+//     let a
+//     console.log(a)
+//     a=9
+//     console.log(a)
+//     const b=98
+//     console.log(b)
+// }
+// types()
+
+// function tmp(){
+    
+//     const d=987;
+//     console.log(d);
+// }
+// tmp();
+// d=90
+// console.log(d);
+
+// function tempy(){
+//     console.log(g)
+// }
+// tempy()
+
+// let e=10;
+// console.log(typeof e);
+
+
+// function myfun(a,b){
+//     return a*b;
+// }
+// let x=myfun(4,3);
+// console.log(x);
+
+// let k='7.79'
+// console.log(typeof k)
